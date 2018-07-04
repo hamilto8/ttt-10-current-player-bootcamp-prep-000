7894d0722e3b9dcfdc6e466454638f5d6a937149
Michael Hamilton <michael.ts.hamilton@gmail.com>
2018-07-04 09:16:16 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,8 +5,13 @@ def turn_count(board)
           counter += 1
         end
     end
+    return counter
 end
 
 def current_player(board)
-
+  if(turn_count(board) % 2 == 0)
+    return "O"
+  else
+    return "X"
+  end
 end
